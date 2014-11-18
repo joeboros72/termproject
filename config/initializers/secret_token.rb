8cbff1e9d79e16fd80ed72ccfcd74eada796653b
Joe B <joeboros72@gmail.com>
2014-11-17 19:29:32 -0500

    initialize Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Termproject::Application.config.secret_token = '82b0304da5dec9b3e2b56e97670f21e6eb9af7ffcc58627aa31f7e23ac8e7da63d25771cc26079a75c6e6420b21609fb3fea74884cb57ff42ee3d5bd3aab35e9'
